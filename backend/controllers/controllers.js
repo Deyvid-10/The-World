fd3332e1ac784fb52748258df614db8345e78f90
Deyvid-10 <marmolejodeyvid@gmail.com>
2026-01-04 21:52:32 -0400

    .

--- backend/controllers/controllers.js
+++ backend/controllers/controllers.js
@@ -372,17 +372,17 @@ export class Controller{
 
     getUserInfo = async (req, res) => {
 
-        // const token = req.cookies.tokenSocialSesion;
+        const token = req.cookies.tokenSocialSesion;
         
-        // if(token === undefined){
-        //     return res.send(false)
-        // }
+        if(token === undefined){
+            return res.send(false)
+        }
 
-        // const {users_id: id} = jwt.verify(token, process.env.JWT_SECRET)
+        const {users_id: id} = jwt.verify(token, process.env.JWT_SECRET)
         
         
         try{
-            const userData = await this.Model.getUserData(42)
+            const userData = await this.Model.getUserData(id)
             const {users_img, users_name, users_last_name, users_email, users_bio} = userData[0]
         
             return res.json([{users_img, users_name, users_last_name, users_email, users_bio}])
